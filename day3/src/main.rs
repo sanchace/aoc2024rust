19aeab4dbea26367f785542cbd1b5aaba99bf9d3
Alex Sanchez <sanchezcalexander@gmail.com>
2024-12-05 04:07:04 -0800

    day3 done

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -6,13 +6,22 @@ fn main() {
 
     let re = Regex::new(r"mul\((?<l>\d+),(?<r>\d+)\)|do\(\)|don\'t\(\)").unwrap();
     
-    //let mut count = 0;
+    let mut count = 0;
+    let mut flag = true;
     for mul in re.captures_iter(&contents) {
-        //let left = &mul["l"].parse::<u64>().expect("Should be number");
-        //let right = &mul["r"].parse::<u64>().expect("Should be number");
-        //count += left * right;
         let s = &mul[0];
-        println!("{s}");
+        //println!("{s}");
+        match s {
+            "do()" => {flag = true},
+            "don't()" => {flag = false},
+            _ => {
+                if flag {
+                    let left = &mul["l"].parse::<u64>().expect("Should be number");
+                    let right = &mul["r"].parse::<u64>().expect("Should be number");
+                    count += left * right;
+                }
+            },
+        }
     }
-    //println!("{count}");
+    println!("{count}");
 }
